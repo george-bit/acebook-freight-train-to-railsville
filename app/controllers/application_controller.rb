170b61c2cedbe426b97d706b690557ddbcc4e37a
Joe <josephhull676@yahoo.co.uk>
2020-06-16 12:49:16 +0100

    Added functionality that will allow users to provide a username and login with it or their email address. Username now displayed in the posts index view next to the post.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@
 # :nodoc:
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
   rescue_from CanCan::AccessDenied do |exception|
     flash[:alert] = 'Access denied'
     redirect_to root_url
+  end
+
+  protected
+  
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
